5f6da60c4f05d8b0c654c60bab0b14ff242cc740
petr.ryabinin <petr.ryabinin@mynd.co>
2018-06-18 22:54:34 +0200

    w

--- core/state_configurations.py
+++ core/state_configurations.py
@@ -1,6 +1,6 @@
 # this file consist of different state configurations
 import numpy as np
-from math import factorial, exp
+from math import factorial, exp, sqrt
 
 DEF_SERIES_LEN = 100
 
@@ -18,9 +18,14 @@ def coherent_state(series_length=DEF_SERIES_LEN, alpha=1):
     return state
 
 
-# TODO
-def squeezed_vacuum(series_length=DEF_SERIES_LEN, squeezing=1):
-    return 0
+def squeezed_vacuum(series_length=DEF_SERIES_LEN, squeezing_amp=1, squeezing_phase=1):
+    if series_length % 2 != 0:
+        print('Warning: series length should be even number')
+        series_length = series_length + 1
+    state = np.zeros(series_length)
+    for n in range(int(series_length / 2)):
+        state[2*n] = 1 / np.cosh(squeezing_amp) * (-1)**n * sqrt(factorial(2*n))/(2**n * factorial(n)) * np.exp(1j*n*squeezing_phase) * np.tanh(squeezing_amp)**n
+    return state
 
 
 # TODO
