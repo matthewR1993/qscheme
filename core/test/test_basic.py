aadd762629b422483fe8a773e8444850b81dd870
petr.ryabinin <petr.ryabinin@mynd.co>
2018-08-02 15:05:15 +0200

    w

--- core/test/test_basic.py
+++ core/test/test_basic.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy.testing import assert_array_equal, assert_allclose
 
-from ..state_configurations import single_photon
+from ..state_configurations import single_photon, fock_state
 from ..basic import *
 
 
@@ -19,12 +19,33 @@ def test_bs2x2_transform():
 
 
 def test_two_bs2x4_transform():
-    series_length = 2
+    series_length = 3
+    t1, r1 = sqrt(0.57), sqrt(0.43)
+    t2, r2 = sqrt(0.17), sqrt(0.83)
+
+    # Two single photons.
     state1 = np.tensordot(single_photon(series_length), single_photon(series_length), axes=0)
-    t1, r1 = 0, 1
-    t2, r2 = 0, 1
-    two_bs2x4_transform(t1, r1, t2, r2, state1)
-    pass
+    out_state1 = np.zeros((series_length,) * 4, dtype=complex)
+    out_state1[0, 1, 0, 1] = t1 * t2
+    out_state1[0, 1, 1, 0] = 1j * t1 * r2
+    out_state1[1, 0, 0, 1] = 1j * r1 * t2
+    out_state1[1, 0, 1, 0] = - r1 * r2
+    assert_array_equal(two_bs2x4_transform(t1, r1, t2, r2, state1), out_state1)
+
+    # Two Fock states with n=2.
+    state2 = np.tensordot(fock_state(2, series_length), fock_state(2, series_length), axes=0)
+    out_state2 = np.zeros((series_length,) * 4, dtype=complex)
+    out_state2[0, 2, 0, 2] = t1**2 * t2**2
+    out_state2[0, 2, 1, 1] = 2j * t1**2 * t2 * r2
+    out_state2[0, 2, 2, 0] = - t1**2 * r2**2
+    out_state2[1, 1, 0, 2] = 2j * t1 * r1 * t2**2
+    out_state2[1, 1, 1, 1] = - 4 * t1 * r1 * t2 * r2
+    out_state2[1, 1, 2, 0] = - 2j * t1 * r1 * r2**2
+    out_state2[2, 0, 0, 2] = - r1**2 * t2**2
+    out_state2[2, 0, 1, 1] = - 2j * r1**2 * t2 * r2
+    out_state2[2, 0, 2, 0] = r1**2 * r2**2
+    out_state2 = out_state2 * 0.5  # initial state is unapplied
+    assert_allclose(two_bs2x4_transform(t1, r1, t2, r2, state2), out_state2)
 
 
 def test_detection():
