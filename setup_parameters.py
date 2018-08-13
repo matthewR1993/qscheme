02cde7c13f9fa8291b8b2ec754fdb4a3a7b0086c
Matvei Riabinin <matvei@dhcp-n3physik-73.uni-paderborn.de>
2018-08-13 13:12:16 +0200

    w

--- setup_parameters.py
+++ setup_parameters.py
@@ -1,27 +1,13 @@
 from math import sqrt
 
 
-# Default scheme parameters
+# Default scheme parameters.
 
-# Set up default scheme parameters.
-# These r,t,a are from 0 to 1. The ideal beam splitter without absorption: t^2 + r^2 = 1
-t1 = sqrt(0.5)
-r1 = sqrt(1 - pow(t1, 2))
-
-t2 = sqrt(0.5)
-r2 = sqrt(1 - pow(t2, 2))
-
-t3 = sqrt(0.5)
-r3 = sqrt(1 - pow(t3, 2))
-
-t4 = sqrt(0.5)
-r4 = sqrt(1 - pow(t4, 2))
-
-# Types of detector
+# Types of detector.
 DET_TYPE = 'IDEAL'
 # DET_TYPE = 'REAL'
 
-# Detector's single photon detection efficiency
+# Detector's single photon detection efficiency.
 SPD = 1
 
 # EPR variance for vacuum state, the very minimum possible.
