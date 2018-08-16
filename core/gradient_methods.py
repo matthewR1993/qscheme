df6a5b9d5b290664b0110044aee0de36c8d71476
petr.ryabinin <petr.ryabinin@mynd.co>
2018-08-17 01:38:34 +0200

    w

--- core/gradient_methods.py
+++ core/gradient_methods.py
@@ -1,13 +1,15 @@
 from core.sytem_setup import *
+from core.squeezing import *
 
 
 # Maximum number of iterations for a gradient descent..
 SEARCH_ITER_MAX = 40
 
+PAR_KEYS = ['t1', 't2', 't3', 't4']
+
 
 # TODO
 def gradient_descent(quantity, start_point, state, phase_diff, det_event, delta=1e-7):
-    raise NotImplementedError
     bs_params = {
         't1': start_point[0],
         'r1': sqrt(1 - start_point[0]**2),
@@ -18,31 +20,41 @@ def gradient_descent(quantity, start_point, state, phase_diff, det_event, delta=
         't3': start_point[2],
         'r3': sqrt(1 - start_point[2]**2),
     }
-    par_keys = bs_params.keys()
 
     # Start point.
     bs_params_arr = [bs_params]
     dm, _ = process_all(state, bs_params_arr[0], phase_diff=phase_diff, det_event=det_event)
-    dm_array = [dm]
-    t_array = [np.array([bs_params['t1'], bs_params['t2'], bs_params['t3'], bs_params['t4']])]
 
-    # todo
-    size = 4
-
+    # Calculating gradients.
     for i in range(1, SEARCH_ITER_MAX):
-        # Calculating gradients.
-        t_array[i] = np.array([t_array[i - 1][0], ])
         dm, _ = process_all(state, bs_params_arr[i], phase_diff=phase_diff, det_event=det_event)
-        bs_params_upp = []
+        if quantity in ['epr_x', 'epr_p']:
+            epr_x, epr_p = erp_squeezing_correlations(dm)
+        elif quantity in ['quadr_x', 'quadr_p']:
+            dX, dP = squeezing_quadratures(dm, channel=1)
 
-        for j in range(size):
+        grads = np.zeros(len(PAR_KEYS), dtype=complex)
+        for j in range(len(PAR_KEYS)):
             bs_params_upp = bs_params_arr[i].copy()
-            bs_params_upp['t1'] = bs_params_upp['t1'] + 0
-            dm_up1, _ = process_all(state, bs_params_arr[i], phase_diff=phase_diff, det_event=det_event)
+            bs_params_upp[PAR_KEYS[j]] += delta
+            dm_up, _ = process_all(state, bs_params_upp, phase_diff=phase_diff, det_event=det_event)
+            if quantity is 'epr_x':
+                epr_x_up, _ = erp_squeezing_correlations(dm_up)
+                grads[j] = (epr_x_up - epr_x) / delta
+            elif quantity is 'epr_p':
+                _, epr_p_up = erp_squeezing_correlations(dm_up)
+                grads[j] = (epr_p_up - epr_p) / delta
+            elif quantity is 'quadr_x':
+                dX_up, _ = squeezing_quadratures(dm_up, channel=1)
+                grads[j] = (dX_up - dX) / delta
+            elif quantity is 'quadr_p':
+                _, dP_up = squeezing_quadratures(dm_up, channel=1)
+                grads[j] = (dP_up - dP) / delta
 
-        dm_array[i] = dm
+        # build next step summing up gradient.
+        bs_params_next = bs_params_arr[i].copy()
+        bs_params_next['t1'] += 0
+        bs_params_arr[i + 1] = bs_params_next
 
-        if quantity is 'epr x':
-            pass
 
     return 0
