2513ce52256e6bbf2ddb7750768445f2b3368b8b
petr.ryabinin <petr.ryabinin@mynd.co>
2018-08-27 04:52:45 +0200

    w

--- core/optimized/setup.py
+++ core/optimized/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 import os
 
-os.environ['CFLAGS'] = '-Ofast -frename-registers -funroll-loops -Wall -Wextra -ffast-math -fno-signed-zeros' + \
+os.environ['CFLAGS'] = '-Ofast -frename-registers -funroll-loops -Wall -Wextra -ffast-math -fno-signed-zeros ' + \
                        '-ffinite-math-only -fno-trapping-math -flto -fcx-limited-range'
 
 setup(
