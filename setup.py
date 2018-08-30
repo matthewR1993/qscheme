2fc80a5c6b596dbee0d50863e46a45753015393e
petr.ryabinin <petr.ryabinin@mynd.co>
2018-08-30 23:58:46 +0200

    w

--- setup.py
+++ setup.py
@@ -1,28 +1,19 @@
 from distutils.core import setup
 from distutils.extension import Extension
+from Cython.Build import cythonize
 import numpy as np
 
-try:
-    from Cython.Distutils import build_ext
-except ImportError:
-    use_cython = False
-else:
-    use_cython = True
-
-cmdclass = {}
-ext_modules = []
-
-if use_cython:
-    ext_modules += [
-        Extension("qscheme.core.optimized", ["core/optimized/transformations.pyx"]),
-    ]
-    cmdclass.update({'build_ext': build_ext})
+extensions = [
+    Extension(
+        name='transformations',
+        sources=['core/src/transformations.pyx']),
+]
 
 setup(
-    name='qscheme',
-    cmdclass=cmdclass,
-    ext_modules=ext_modules,
+    name="qscheme",
+    ext_modules=cythonize(extensions),
     include_dirs=[np.get_include()],
     extra_compile_args=['-Ofast', '-frename-registers', '-funroll-loops', '-Wall', '-Wextra', '-ffast-math',
-                        '-fno-signed-zeros', '-ffinite-math-only', '-fno-trapping-math', '-flto']
+                    '-fno-signed-zeros', '-ffinite-math-only', '-fno-trapping-math', '-flto']
+
 )
