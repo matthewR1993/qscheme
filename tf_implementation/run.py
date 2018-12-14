96953809ac74ee1c0708fa11e1efe47ae1040240
Matvei Riabinin <matvei@thphys-n.uni-paderborn.de>
2018-12-14 16:14:09 +0100

    w

--- tf_implementation/run.py
+++ tf_implementation/run.py
@@ -17,16 +17,10 @@ state1 = coherent_state(L, alpha=1.0)
 # state2 = coherent_state(L, alpha=1.0)
 state2 = single_photon(L)
 
-I = tf.constant(1j, tf.complex128)
-
 st1_tf = tf.constant(state1, tf.complex128)
 st2_tf = tf.constant(state2, tf.complex128)
 
-# Unapplied input state.
-mut_state = tf.tensordot(st1_tf, st2_tf, axes=0)
-
-T1 = tf.Variable(0.7, trainable=True, dtype=tf.float64)
-# R1 = tf.Variable(0.5, trainable=True)
+I = tf.constant(1j, tf.complex128)
 
 
 def bs_transformation_tf(input_state, T1):
@@ -42,7 +36,7 @@ def bs_transformation_tf(input_state, T1):
         for n in range(sz):
             for k in range(m + 1):
                 for l in range(n + 1):
-                    fact_const[m, n, k, l] = (1j) ** (k + l) * factorial(m) * factorial(n) / (
+                    fact_const[m, n, k, l] = 1j ** (k + l) * factorial(m) * factorial(n) / (
                                 factorial(k) * factorial(m - k) * factorial(l) * factorial(n - l))
 
     bs_const_tf = tf.constant(fact_const, tf.complex128)
@@ -80,15 +74,48 @@ def bs_transformation_tf(input_state, T1):
     c2 = tf.tensordot(tf.ones([sz * 2 - 1, sz * 2 - 1], tf.complex128), c1, axes=0)
     d = tf.multiply(p1, c2)
 
-    state_out = tf.reduce_sum(d, axis=[2, 3, 4, 5])
-    return state_out
+    return tf.reduce_sum(d, axis=[2, 3, 4, 5])
 
 
-state_out = bs_transformation_tf(mut_state, T1)
+def phase_mod(phase, input, input_type, channel):
+    sz = input.shape.dims[0].value
+    if input_type == 'state':
+        m = np.resize(np.arange(0, sz), (sz, sz))
+        if channel == 1:
+            m = m.T
+        elif channel == 2:
+            pass
+        else:
+            raise ValueError
+        pow_m = tf.constant(m, tf.complex128)
+        return tf.multiply(input, tf.pow(tf.exp(I * tf.cast(phase, tf.complex128)), pow_m))
+    elif input_type == 'dm':
+        raise NotImplementedError
+    else:
+        raise ValueError
 
-state_trace = tf.trace(tf.abs(state_out))
-# Cost function.
-cost = tf.cast(state_trace, tf.float64)
+
+with tf.name_scope('system') as scope:
+    # Unapplied input state.
+    mut_state = tf.tensordot(st1_tf, st2_tf, axes=0, name='input_state')
+
+    # Trainable parameters.
+    phase = tf.Variable(0.1 * np.pi, trainable=True, dtype=tf.float64, name='phase')
+    T1 = tf.Variable(0.3, trainable=True, dtype=tf.float64, name='T1')
+    T2 = tf.Variable(0.3, trainable=True, dtype=tf.float64, name='T2')
+
+    s1 = bs_transformation_tf(mut_state, T1)
+    s2 = phase_mod(phase, s1[:L, :L], input_type='state', channel=2)
+    state_out = bs_transformation_tf(s2, T2)
+
+    # Cost function.
+    cost = tf.cast(tf.trace(tf.abs(state_out)), tf.float64, name='cost')
+
+    # Register summaries.
+    # tf.summary.scalar('cost', cost)
+    # tf.summary.scalar('T1', T1)
+    # tf.summary.scalar('T1', T2)
+    # tf.summary.scalar('phase', phase)
 
 
 # WORKS.
@@ -121,10 +148,8 @@ cost = tf.cast(state_trace, tf.float64)
 # plt.imshow(np.abs(state_out_val))
 # plt.show()
 
-
-# Todo: visualise
-# tf.summary.scalar('cost', cost)
-# tf.summary.scalar('T1', T1)
+# tensorboard --logdir=/Users/matvei/PycharmProjects/qscheme/tf_implementation/logs/summaries/log
+# http://localhost:6006
 
 
 optimizer = tf.train.AdamOptimizer()
@@ -141,22 +166,70 @@ cost_progress = []
 
 for i in range(reps):
 
-    [_, cost_val, T1_val] = sess.run([minimize_op, cost, T1])
-    cost_progress.append({'cost': cost_val, 'par_value': T1_val})
+    [_, cost_val, T1_val, T2_val, phase_val] = sess.run([minimize_op, cost, T1, T2, phase])
+    cost_progress.append({'cost': cost_val, 'T1': T1_val, 'T2': T2_val, 'phase': phase_val})
     # cost_progress.append(cost_val)
 
     # Prints progress.
     if i % display_step == 0:
-        print("Rep: {} Cost: {} T1_value: {}".format(i, cost_val, T1_val))
+        print("Rep: {} Cost: {} T1: {} T2: {} phase: {}".format(i, cost_val, T1_val, T2_val, phase_val))
+
+
+# summaries_dir = '/Users/matvei/PycharmProjects/qscheme/tf_implementation/logs/summaries'
+# merged = tf.summary.merge_all()
+# train_writer = tf.summary.FileWriter(summaries_dir + '/log', sess.graph)
 
 
 plt.plot([c['cost'] for c in cost_progress])
+plt.xlabel('cost')
+plt.xlabel('step')
 plt.show()
 
-plt.plot([c['par_value'] for c in cost_progress])
-plt.show()
+# plt.plot([c['par_value'] for c in cost_progress])
+# plt.show()
 
-pd.DataFrame(cost_progress).plot()
+# pd.DataFrame(cost_progress).plot()
 
 
+# Detection:
+# def detection(state, type):
+#     return 0
 
+
+def bs_2x4_transform(T1, T2, input_state):
+    return 0
+
+
+# def two_bs2x4_transform(t1, r1, t2, r2, input_state):
+#     """
+#     Transformation at 2 beam splitters.
+#     Two input channels and four output channles - 2x4 transformation.
+#     Creation operators transformation:
+#     a1 => t1 a2 + i r1 a1.
+#     a2 => t2 a4 + i r2 a3.
+#     With transmission and reflection coefficients:
+#     t1^2 + r1^2 = 1.
+#     t2^2 + r2^2 = 1.
+#     :param t1: BS1 transmission.
+#     :param r1: BS1 reflection.
+#     :param t2: BS2 transmission.
+#     :param r2: BS2 reflection.
+#     :param input_state: Two channels(modes) unapllied state.
+#     :return: Four channels(modes) unapllied state.
+#     """
+#     size = len(input_state)
+#     output_state = np.zeros((size,) * 4, dtype=complex)
+#     for m in range(size):
+#         for n in range(size):
+#
+#             for k in range(m + 1):
+#                 for l in range(n + 1):
+#                     # channels indexes
+#                     ind1 = k
+#                     ind2 = m - k
+#                     ind3 = l
+#                     ind4 = n - l
+#                     coeff = input_state[m, n] * t1**(m - k) * (1j*r1)**k * t2**(n - l) * (1j*r2)**l * factorial(m) * factorial(n) / (factorial(k) * factorial(m - k) * factorial(l) * factorial(n - l))
+#                     output_state[ind1, ind2, ind3, ind4] = output_state[ind1, ind2, ind3, ind4] + coeff
+#
+#     return output_state
